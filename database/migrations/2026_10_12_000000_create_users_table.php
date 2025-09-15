8677972254362e8c4a7adc8f4ff45e16acb67eab
izzoel <ramadhani.zulfahmi@gmail.com>
2025-09-16 01:29:47 +0800

    ! migration users

--- database/migrations/2026_10_12_000000_create_users_table.php
+++ database/migrations/2026_10_12_000000_create_users_table.php
@@ -14,6 +14,7 @@ return new class extends Migration {
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('avatar');
             $table->string('name');
             $table->string('password');
             $table->foreignId('id_role')->constrained('roles')->onDelete('cascade');
@@ -22,6 +23,7 @@ return new class extends Migration {
 
         DB::table('users')->insert([
             'id' => 1,
+            'avatar' => rand(0, 11) . '.png',
             'name' => 'admin',
             'password' => Hash::make('nimda'),
             'id_role' => 1,
