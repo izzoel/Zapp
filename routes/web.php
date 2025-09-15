68594c6c8f638a9b5699a980f80667f2ad6cd54b
izzoel <ramadhani.zulfahmi@gmail.com>
2025-09-16 01:34:24 +0800

    ! web

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
-use App\Livewire\Backend\Icon;
+use App\Livewire\Backend\Akses;
 use App\Livewire\Backend\Menu;
+use App\Livewire\Backend\User;
+use App\Livewire\Backend\Role;
+use App\Livewire\Backend\Icon;
 use App\Livewire\Backend\Admin;
 use App\Http\Controllers\LandingController;
 use Illuminate\Support\Facades\Route;
@@ -10,9 +13,9 @@ Route::any('/', [LandingController::class, 'landing'])->name('landing');
 Route::any('/login', [LandingController::class, 'login'])->name('login');
 Route::any('/logout', [LandingController::class, 'logout'])->name('logout');
 
-Route::get('/akses', \App\Livewire\Backend\Akses::class)->middleware('r');
-Route::get('/menu', \App\Livewire\Backend\Menu::class)->middleware('r');
-Route::get('/user', \App\Livewire\Backend\User::class)->middleware('r');
-Route::get('/role', \App\Livewire\Backend\Role::class)->middleware('r');
-Route::get('/admin', \App\Livewire\Backend\Admin::class);
-Route::get('/icon', \App\Livewire\Backend\Icon::class);
+Route::get('/akses', Akses::class)->middleware('r');
+Route::get('/menu', Menu::class)->middleware('r');
+Route::get('/user', User::class)->middleware('r');
+Route::get('/role', Role::class)->middleware('r');
+Route::get('/admin', Admin::class);
+Route::get('/icon', Icon::class);
